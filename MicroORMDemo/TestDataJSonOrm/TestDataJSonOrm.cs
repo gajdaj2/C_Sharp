443e940b48ff85a2c8470bfbe7fdb699a1aa807b
Kuba <horda1@op.pl>
2017-09-23 16:12:16 +0200

    Adding new Project for TestData Managment by ORM

---
+++ MicroORMDemo/TestDataJSonOrm/TestDataJSonOrm.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDataJSonOrm.DataModel;
+using TestDataJSonOrm.IModel;
+using TestDataJSonOrm.TestDataRepository;
+
+namespace TestDataJSonOrm
+{
+    [TestFixture]
+    public class TestDataJSonOrm
+    {
+        [Test]
+        public void ShouldAddNewTestData()
+        {
+            TestDataOperation repo = CreateNewTestDataOperation();
+            TestData dataa = new TestData();
+            dataa.Build_Id = 111;
+            dataa.Test_Data = "1312312312312312";
+            dataa.Team = "Phoenix";
+            repo.Add(dataa);
+        }
+
+
+        private TestDataOperation CreateNewTestDataOperation()
+        {
+            return new TestDataRepository.TestDataOperation();
+        }
+    }
+}
